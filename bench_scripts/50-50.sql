3661e782968a402d68c8e56d965393a1eba115dd
vicente-simoes <165936060+vicente-simoes@users.noreply.github.com>
2025-10-19 19:29:19 +0100

    Parameterize aid values in SQL script

--- bench_scripts/50-50.sql
+++ bench_scripts/50-50.sql
@@ -1,5 +1,7 @@
 -- simple mix: one update and one select per transaction
+\set aid1 (1 + random() * 1000000)::int
+\set aid2 (1 + random() * 1000000)::int
 BEGIN;
-UPDATE pgbench_accounts SET abalance = abalance + 1 WHERE aid = random_between(1, 1000000);
-SELECT abalance FROM pgbench_accounts WHERE aid = random_between(1, 1000000);
+UPDATE pgbench_accounts SET abalance = abalance + 1 WHERE aid = :aid1;
+SELECT abalance FROM pgbench_accounts WHERE aid = :aid2;
 COMMIT;
