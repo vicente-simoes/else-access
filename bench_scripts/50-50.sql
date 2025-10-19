06be9ade6b83c33758afe5873e471fe71f863d93
vicente-simoes <165936060+vicente-simoes@users.noreply.github.com>
2025-10-19 19:33:00 +0100

    Fix SQL variable declaration syntax in 50-50.sql

--- bench_scripts/50-50.sql
+++ bench_scripts/50-50.sql
@@ -1,6 +1,6 @@
 -- simple mix: one update and one select per transaction
-\set aid1 (1 + random() * 1000000)::int
-\set aid2 (1 + random() * 1000000)::int
+\set aid1 int(1 + random() * 1000000)
+\set aid2 int(1 + random() * 1000000)
 BEGIN;
 UPDATE pgbench_accounts SET abalance = abalance + 1 WHERE aid = :aid1;
 SELECT abalance FROM pgbench_accounts WHERE aid = :aid2;
